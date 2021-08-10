999bd663c3d8524e95d02a05c77e4a7b011d7f19
GuilhermyHenrique-Dev <guilhermy.castilho@hotmail.com>
2021-08-09 21:08:30 -0300

    create modules

---
+++ src/app/views/navigation-history/navigation-history.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation-history',
+  templateUrl: './navigation-history.component.html',
+  styleUrls: ['./navigation-history.component.scss']
+})
+export class NavigationHistoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
